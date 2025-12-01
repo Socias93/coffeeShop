96a80dbc586b17ee9594e624e205c36c6249b0ae
Christian Socias <Socias_93@hotmail.com>
2025-12-01 23:01:42 +0100

    upgraded style for viewcoffe page

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,11 @@
-import { StrictMode } from "react";
-import { createRoot } from "react-dom/client";
 import "./index.css";
+import "./app.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "@fortawesome/fontawesome-free/css/all.css";
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
 import { RouterProvider } from "react-router-dom";
 import { router } from "./routes.tsx";
-import "./app.css";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
--- src/pages/ViewCoffeePage.tsx
+++ src/pages/ViewCoffeePage.tsx
@@ -49,77 +49,104 @@ function ViewCoffeePage() {
 
   return (
     <>
-      <div className="row bg-dark text-center">
-        <div className="col">
-          {id === "new" ? (
-            <img
-              className="vh-100 w-100"
-              src="/bilder/4coffee.webp"
-              alt="Coffee Picture"
-            />
-          ) : (
-            <img
-              className="vh-100 w-100"
-              src={recipe?.imageUrl}
-              alt="Coffee Picture"
-            />
-          )}
-        </div>
-        <div className="col vh-100 d-grid justify-content-center align-content-center">
-          <form
-            onSubmit={handleSubmit(onSubmit)}
-            className="w-100"
-            style={{ maxWidth: 700 }}>
+      <div className="app-container bg-dark">
+        <div className="row">
+          <div className="col-md-5 d-flex flex-column align-items-center mb-3 mb-md-0">
             {id === "new" ? (
-              <h4 className="mb-3">Create a Recipe</h4>
+              <img
+                className="w-100 shadow-lg rounded-4 mb-3"
+                src="/bilder/4coffee.webp"
+                alt="Coffee Picture"
+                style={{ objectFit: "cover" }}
+              />
             ) : (
-              <h4>Recipe {id}</h4>
-            )}
-
-            <div className="shadow rounded-4 p-4 bg-dark">
-              <div className="mb-3">
-                <label className="form-label text-light">Title</label>
-                <input {...register("title")} className="form-control" />
-                {errors.title && (
-                  <p className="text-danger">{errors.title.message}</p>
-                )}
-              </div>
-
-              <div className="mb-3">
-                <label className="form-label text-light">Description</label>
-                <input {...register("description")} className="form-control" />
-                {errors.description && (
-                  <p className="text-danger">{errors.description.message}</p>
-                )}
-              </div>
-
-              <div className="mb-3">
-                <label className="form-label text-light">Ingredients</label>
-                <input {...register("ingredients")} className="form-control" />
-                {errors.ingredients && (
-                  <p className="text-danger">{errors.ingredients.message}</p>
-                )}
-              </div>
-
-              <div className="mb-3">
-                <label className="form-label text-light">Image</label>
-                <input
-                  {...register("imageUrl")}
-                  type="text"
-                  className="form-control"
+              <>
+                <img
+                  className="w-100 shadow-lg rounded-4 mb-3"
+                  src={recipe?.imageUrl}
+                  alt="Coffee Picture"
+                  style={{ objectFit: "cover", maxHeight: "400px" }}
                 />
-                {errors.imageUrl && (
-                  <p className="text-danger">{errors.imageUrl.message}</p>
-                )}
-              </div>
 
-              <div className="text-center mt-3">
-                <button type="submit" className="btn btn-outline-light">
-                  Save
-                </button>
-              </div>
+                <div className="p-3 shadow-lg rounded-4 w-100">
+                  <h2>Title - {recipe?.title}</h2>
+                  <h4>Category - {recipe?.category?.name}</h4>
+                  <h5>Description - {recipe?.description}</h5>
+                  <span>Ingredients - {recipe?.ingredients}</span>
+                  <p>Steps - {recipe?.steps}</p>
+                </div>
+              </>
+            )}
+          </div>
+
+          <div className="col-md-2 d-flex align-items-center justify-content-center">
+            <div className="vertical-text text-light text-center">
+              COFFEESHOP
             </div>
-          </form>
+          </div>
+
+          <div className="col-md-5 d-flex justify-content-center">
+            <form
+              onSubmit={handleSubmit(onSubmit)}
+              className="w-100"
+              style={{ maxWidth: 700 }}>
+              {id === "new" ? (
+                <h4 className="mb-3 mt-4 text-light">Create a Recipe</h4>
+              ) : (
+                <h4 className="mt-4 text-light">Recipe {id}</h4>
+              )}
+
+              <div className="shadow-lg rounded-4 p-4 bg-dark">
+                <div className="mb-3">
+                  <label className="form-label text-light">Title</label>
+                  <input {...register("title")} className="form-control" />
+                  {errors.title && (
+                    <p className="text-danger">{errors.title.message}</p>
+                  )}
+                </div>
+
+                <div className="mb-3">
+                  <label className="form-label text-light">Description</label>
+                  <input
+                    {...register("description")}
+                    className="form-control"
+                  />
+                  {errors.description && (
+                    <p className="text-danger">{errors.description.message}</p>
+                  )}
+                </div>
+
+                <div className="mb-3">
+                  <label className="form-label text-light">Ingredients</label>
+                  <input
+                    {...register("ingredients")}
+                    className="form-control"
+                  />
+                  {errors.ingredients && (
+                    <p className="text-danger">{errors.ingredients.message}</p>
+                  )}
+                </div>
+
+                <div className="mb-3">
+                  <label className="form-label text-light">Image</label>
+                  <input
+                    {...register("imageUrl")}
+                    type="text"
+                    className="form-control"
+                  />
+                  {errors.imageUrl && (
+                    <p className="text-danger">{errors.imageUrl.message}</p>
+                  )}
+                </div>
+
+                <div className="text-center mt-3">
+                  <button type="submit" className="btn btn-outline-light">
+                    Save
+                  </button>
+                </div>
+              </div>
+            </form>
+          </div>
         </div>
       </div>
     </>
