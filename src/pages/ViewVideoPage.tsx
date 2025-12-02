fbd2346a8473fc7dc77b5040a71eda1e3c1effbf
Christian Socias <Socias_93@hotmail.com>
2025-12-02 20:52:03 +0100

    added img and form on viewvideopage

--- src/pages/ViewVideoPage.tsx
+++ src/pages/ViewVideoPage.tsx
@@ -1,5 +1,46 @@
+import { zodResolver } from "@hookform/resolvers/zod";
+import { schema, videoData } from "./schemas/VideoSchema";
+import { useForm } from "react-hook-form";
+
 function ViewVideoPage() {
-  return <h1>Video</h1>;
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<videoData>({ resolver: zodResolver(schema) });
+
+  return (
+    <>
+      <img
+        style={{ width: "100%", height: "45vh" }}
+        src="/bilder/newnewcoffee.jpg"
+        alt="Coffee Background image"></img>
+      <div className="d-flex justify-content-center">
+        <form>
+          <div className="m-4 p-5 shadow rounded-5">
+            <div className="mt-2">
+              <label className="form-label d-grid justify-content-center mt-1">
+                Title
+              </label>
+              <input className="form-control bg-dark" type="text" />
+            </div>
+            <div className="mt-3">
+              <label className="form-label d-grid justify-content-center mt-1">
+                Video Link
+              </label>
+              <input className="form-control bg-dark" type="text" />
+            </div>
+            <div className="mt-3">
+              <label className="form-label d-grid justify-content-center mt-1">
+                Image
+              </label>
+              <input className="form-control bg-dark" type="text" />
+            </div>
+          </div>
+        </form>
+      </div>
+    </>
+  );
 }
 
 export default ViewVideoPage;
