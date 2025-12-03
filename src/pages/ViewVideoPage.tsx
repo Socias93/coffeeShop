ad792f98fc9839a51def812b8aa8e0ad8b600011
Christian Socias <Socias_93@hotmail.com>
2025-12-03 14:07:07 +0100

    fixed bg colors when errors appear on form, fixed onSubmit function, and error handling on inputs

--- src/pages/ViewVideoPage.tsx
+++ src/pages/ViewVideoPage.tsx
@@ -10,29 +10,63 @@ function ViewVideoPage() {
     formState: { errors },
   } = useForm<videoData>({ resolver: zodResolver(schema) });
 
+  function onSubmit(data: videoData) {
+    console.log("Submitted", data);
+  }
+
   return (
     <>
       <VideoHeaderImage />
       <div className="d-flex justify-content-center">
-        <form>
+        <form onSubmit={handleSubmit(onSubmit)}>
           <div className="m-4 p-5 shadow rounded-5">
             <div className="mt-2">
               <label className="form-label d-grid justify-content-center mt-1">
                 Title
               </label>
-              <input className="form-control bg-dark" type="text" />
+              <input
+                {...register("title")}
+                className={`form-control bg-${
+                  errors.title ? "danger-subtle" : "dark"
+                }`}
+                type="text"
+              />
+              {errors.title && (
+                <p className="text-danger">{errors.title.message} </p>
+              )}
             </div>
             <div className="mt-3">
               <label className="form-label d-grid justify-content-center mt-1">
                 Video Link
               </label>
-              <input className="form-control bg-dark" type="text" />
+              <input
+                {...register("videoUrl")}
+                className={`form-control bg-${
+                  errors.title ? "danger-subtle" : "dark"
+                }`}
+                type="text"
+              />
+              {errors.videoUrl && (
+                <p className="text-danger">{errors.videoUrl.message} </p>
+              )}
             </div>
             <div className="mt-3">
               <label className="form-label d-grid justify-content-center mt-1">
                 Image
               </label>
-              <input className="form-control bg-dark" type="text" />
+              <input
+                {...register("imgaeUrl")}
+                className={`form-control bg-${
+                  errors.title ? "danger-subtle" : "dark"
+                }`}
+                type="text"
+              />
+              {errors.imgaeUrl && (
+                <p className="text-danger">{errors.imgaeUrl.message} </p>
+              )}
+            </div>
+            <div className="text-center mt-4">
+              <button className="btn btn-outline-light">Create</button>
             </div>
           </div>
         </form>
