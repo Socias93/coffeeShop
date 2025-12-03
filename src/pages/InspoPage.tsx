2f92e9c8d844754f665f23cf5c7983793dfce237
Christian Socias <Socias_93@hotmail.com>
2025-12-03 15:33:47 +0100

    made inspopage responsive

--- src/pages/InspoPage.tsx
+++ src/pages/InspoPage.tsx
@@ -26,25 +26,33 @@ function InspoPage() {
       </div>
     );
 
+  const coffeeMugg = <i className="fa-solid fa-mug-saucer"></i>;
+
   return (
     <>
       <div className="container-lg px-3 py-3">
-        <div className="row row-cols-1 row-cols-sm-2 row-cols-md-2 row-cols-lg-3 g-4 justify-content-center">
+        <h1 className="text-center m-3">{coffeeMugg} Coffee inspo</h1>
+        <div className="d-flex row row-cols-1 row-cols-sm-1 row-cols-md-3 justify-content-center g-5">
           {videos.map((video) => (
-            <div key={video.id} className="col d-flex">
+            <div key={video.id} className="col d-flex justify-content-center">
               <div
-                className="p-3 border border-dark rounded-4 m- d-flex flex-column w-100"
-                style={{ minHeight: 320, minWidth: 350 }}>
+                className="shadow-sm m-1 justify-content-center border border-dark rounded-4 d-flex flex-column w-100 h-100"
+                style={{
+                  minHeight: 220,
+                  minWidth: 250,
+                  maxWidth: 500,
+                  maxHeight: 400,
+                }}>
                 <div className="position-relative">
                   <img
                     src={video.imageUrl}
                     alt="Coffee image"
                     className="img-fluid w-100 rounded-3"
-                    style={{ height: 150, objectFit: "cover" }}
+                    style={{ height: 200, objectFit: "cover" }}
                   />
                 </div>
-                <h5 className="mt-2 mb-1">{video.title}</h5>
-                <div className="d-flex justify-content-between mt-auto">
+                <h5 className="text-center mt-2">{video.title}</h5>
+                <div className="d-flex justify-content-between m-4">
                   <button
                     onClick={() => window.open(video.videoUrl)}
                     className="rounded-2"
@@ -72,10 +80,11 @@ function InspoPage() {
           ))}
         </div>
       </div>
-      <div className="text-center">
+      <div className="text-center mt-4">
         <button
           onClick={() => navigate("/create/new/video")}
-          className="btn btn-outline-secondary w-25">
+          className="btn btn-outline-secondary px-4"
+          style={{ height: "48px" }}>
           Create new inspo
         </button>
       </div>
